77fe38ef955ce72a1912d251a55d2f3f0a062deb
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2025-03-04 16:28:25 +0100

    Add TP3

---
+++ TP3/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { json } = require("body-parser");
+const { addResolversToSchema } = require("@graphql-tools/schema");
+const taskSchemaPromise = require("./taskSchema");
+const taskResolver = require("./taskResolver");
+const app = express();
+
+async function setupServer() {
+  try {
+    const PORT = process.env.PORT || 5000;
+
+    const taskSchema = await taskSchemaPromise;
+
+    const schemaWithResolvers = addResolversToSchema({
+      schema: taskSchema,
+      resolvers: taskResolver,
+    });
+
+    const server = new ApolloServer({
+      schema: schemaWithResolvers,
+    });
+
+    await server.start();
+
+    app.use("/graphql", json(), expressMiddleware(server));
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.error("Failed to start the Apollo server:", error);
+  }
+}
+setupServer();
